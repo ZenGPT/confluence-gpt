5ae7a1d80359d3d7de9c22c73cedda678077d3eb
MrCoder <eagle.xiao@gmail.com>
2023-03-05 10:46:15 +1100

    Use JSX.

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,11 @@ export default function routes(app, addon) {
 
     // This is an example route used by "generalPages" module (see atlassian-connect.json).
     // Verify that the incoming request is authenticated with Atlassian Connect.
-    app.get('/hello-world', addon.authenticate(), (req, res) => {
+    app.get('/hello-world', (req, res) => {
         // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
         // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
         res.render(
-          'hello-world.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
+          'hello-world.jsx', // change this to 'hello-world.jsx' to use the Atlaskit & React version
           {
             title: 'Atlassian Connect'
             //, issueId: req.query['issueId']
