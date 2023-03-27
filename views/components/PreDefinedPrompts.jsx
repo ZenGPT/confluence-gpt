bd3a5878a27e2f94e81d2259357d64609d509605
unimu-cic <kkcarson1874@gmail.com>
2023-03-27 21:03:02 +0800

    feat: add predefined prompts component

---
+++ views/components/PreDefinedPrompts.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PromptCard from './PromptCard';
+import { PRE_DEFINED_PROMPTS } from '../constant/prompt';
+import styled from 'styled-components';
+
+const ListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 220px;
+  border-right: 1px solid #eeeeee;
+  padding-right: 16px;
+`;
+
+const StyledHeading = styled.h3`
+  margin-bottom: 10px;
+  font-size: 16px;
+`;
+
+const PreDefinedPrompts = ({ onSelect }) => {
+  return (
+    <ListWrapper>
+      <StyledHeading level='h400'>Predefined Prompts</StyledHeading>
+      {PRE_DEFINED_PROMPTS.map((prompt) => (
+        <PromptCard key={prompt} content={prompt} onClick={onSelect}/>
+      ))}
+    </ListWrapper>
+  );
+};
+
+export default PreDefinedPrompts;
