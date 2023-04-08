d7d623a4ea411af62d4017ad55c544c72967a51b
MrCoder <eagle.xiao@gmail.com>
2023-04-08 13:06:45 +1000

    Fixed vertical scroll.

--- views/hello-world.jsx
+++ views/hello-world.jsx
@@ -24,18 +24,23 @@ const Page = styled.div`
   align-items: center;
   justify-content: start;
   height: 100vh;
-  width: 100vw;
+  width: 1024px;
+  margin: 0 20px;
 `;
 
 const Wrapper = styled.div`
   display: flex;
-  max-width: 768px;
+  height: 100%;
+  width: 100%;
   margin: 0 auto;
-  max-height: 768px;
   overflow-y: auto;
 `;
 
 const ChatBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  height: 100%;
   width: 100%;
   margin: 0 auto;
   padding-left: 16px;
@@ -153,7 +158,7 @@ const FormDefaultExample = () => {
 
   return (
     <Page>
-      <DebugComponent/>
+      {/*<DebugComponent/>*/}
       <Wrapper>
         <PreDefinedPrompts onSelect={handleSelect}/>
         <ChatBox>
@@ -199,22 +204,24 @@ const FormDefaultExample = () => {
             )}
           </Form>
           {/* TODO: integrate with the API response */}
-          <div style={{display: 'flex', justifyContent: 'flex-start'}}>
+          <div style={{width: '100%', display: 'flex', flexDirection: 'flex-row', justifyContent: 'flex-start', overflowY: 'hidden'}}>
+            <div className={'content'} style={{flexGrow: 1, overflowY: 'scroll'}}>
+              <ReactMarkdown children={messages}/>
+            </div>
             {messages && (
-              <InlineDialog
-                onClose={() => setCopied(false)}
-                content='Copied Successful'
-                isOpen={copied}
-              >
-                <Button
-                  ref={copyBtnRef}
-                  onClick={handleCopy}
-                  iconBefore={<CopyIcon label='' size='medium'/>}
-                />
-              </InlineDialog>
+                <InlineDialog
+                    onClose={() => setCopied(false)}
+                    content='Copied Successful'
+                    isOpen={copied}
+                >
+                  <Button
+                      ref={copyBtnRef}
+                      onClick={handleCopy}
+                      iconBefore={<CopyIcon label='' size='medium'/>}
+                  />
+                </InlineDialog>
             )}
           </div>
-          <ReactMarkdown children={messages}/>
         </ChatBox>
       </Wrapper>
     </Page>
