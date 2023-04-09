96bcad0f051e74927bdfef90927097118000cd4c
MrCoder <eagle.xiao@gmail.com>
2023-04-09 18:18:48 +1000

    Moved createMockReader function to global.

--- views/StreamProcessor/StreamProcessor.test.mjs
+++ views/StreamProcessor/StreamProcessor.test.mjs
@@ -1,32 +1,32 @@
 // processStream.test.mjs
-import { processStream } from './StreamProcessor.mjs';
+import {processStream} from './StreamProcessor.mjs';
 
+const createMockReader = (data) => {
+  let readIndex = 0;
+
+  return {
+    // The test data will be sent one record per read
+    read: async () => {
+      if (readIndex < data.length) {
+        const value = new TextEncoder().encode(data[readIndex]);
+        readIndex += 1;
+        return { value, done: false };
+      } else {
+        return { value: undefined, done: true };
+      }
+    },
+  };
+};
 
 test('processStream handles incomplete data correctly', async () => {
-  const createMockReader = () => {
-    const testData = [
-      'data: {"id": "chatcmpl-72vOaELB8DHU8lf4W8I6Odxv2XsGh", "object": "chat.completion.chunk", "created": 1680931640, "model": "gpt-3.5-turbo-0301", "choices": [{"delta": {"content": " where"}, "index": 0, "finish_reason": null}]}\n' +
-      'data: {"id": "chatcmpl-72vOaELB8DHU8lf4W8I6Odxv2XsGh", "object": "chat.completion.chunk", "created": 1680931640, "model": "gpt-3.5-turbo-0301", "choices": [{"delta": {"content": " are"}, "index": 0, "finish_reason": null}]}\n' +
-      'data: {"id": "chatcmpl-72vOaELB8DHU8lf4W8I6Odxv2XsGh"',
-      ', "object": "chat.completion.chunk", "created": 1680931640, "model": "gpt-3.5-turbo-0301", "choices": [{"delta": {"content": " you"}, "index": 0, "finish_reason": null}]}\n',
-    ];
+  const testData = [
+    'data: {"id": "chatcmpl-72vOaELB8DHU8lf4W8I6Odxv2XsGh", "object": "chat.completion.chunk", "created": 1680931640, "model": "gpt-3.5-turbo-0301", "choices": [{"delta": {"content": " where"}, "index": 0, "finish_reason": null}]}\n' +
+    'data: {"id": "chatcmpl-72vOaELB8DHU8lf4W8I6Odxv2XsGh", "object": "chat.completion.chunk", "created": 1680931640, "model": "gpt-3.5-turbo-0301", "choices": [{"delta": {"content": " are"}, "index": 0, "finish_reason": null}]}\n' +
+    'data: {"id": "chatcmpl-72vOaELB8DHU8lf4W8I6Odxv2XsGh"',
+    ', "object": "chat.completion.chunk", "created": 1680931640, "model": "gpt-3.5-turbo-0301", "choices": [{"delta": {"content": " you"}, "index": 0, "finish_reason": null}]}\n',
+  ];
 
-    let readIndex = 0;
-
-    return {
-      read: async () => {
-        if (readIndex < testData.length) {
-          const value = new TextEncoder().encode(testData[readIndex]);
-          readIndex += 1;
-          return { value, done: false };
-        } else {
-          return { value: undefined, done: true };
-        }
-      },
-    };
-  };
-
-  const mockReader = createMockReader();
+  const mockReader = createMockReader(testData);
   let receivedText = '';
 
   const messageCallback = (text) => {
@@ -39,31 +39,13 @@ test('processStream handles incomplete data correctly', async () => {
 });
 
 test('data closed with [DONE]', async () => {
-  console.log('data closed with [DONE]');
-  const createMockReader = () => {
-    const testData = [
-      'data: {"id": "chatcmpl-72vOaELB8DHU8lf4W8I6Odxv2XsGh", "object": "chat.completion.chunk", "created": 1680931640, "model": "gpt-3.5-turbo-0301", "choices": [{"delta": {"content": " where"}, "index": 0, "finish_reason": null}]}\n' +
-      'data: {"id": "chatcmpl-72vOaELB8DHU8lf4W8I6Odxv2XsGh", "object": "chat.completion.chunk", "created": 1680931640, "model": "gpt-3.5-turbo-0301", "choices": [{"delta": {"content": " are"}, "index": 0, "finish_reason": null}]}\n' +
-      'data: [DONE]\n',
-    ];
+  const testData = [
+    'data: {"id": "chatcmpl-72vOaELB8DHU8lf4W8I6Odxv2XsGh", "object": "chat.completion.chunk", "created": 1680931640, "model": "gpt-3.5-turbo-0301", "choices": [{"delta": {"content": " where"}, "index": 0, "finish_reason": null}]}\n' +
+    'data: {"id": "chatcmpl-72vOaELB8DHU8lf4W8I6Odxv2XsGh", "object": "chat.completion.chunk", "created": 1680931640, "model": "gpt-3.5-turbo-0301", "choices": [{"delta": {"content": " are"}, "index": 0, "finish_reason": null}]}\n' +
+    'data: [DONE]\n',
+  ];
 
-    let readIndex = 0;
-
-    return {
-      read: async () => {
-        if (readIndex < testData.length) {
-          const value = new TextEncoder().encode(testData[readIndex]);
-          readIndex += 1;
-          return { value, done: false };
-        } else {
-          return { value: undefined, done: true };
-        }
-      },
-    };
-  };
-
-
-  const mockReader = createMockReader();
+  const mockReader = createMockReader(testData);
   let receivedText = '';
 
   const messageCallback = (text) => {
