6230a7d313bcc43d936d3ade2837c8a91870e0ce
MrCoder <eagle.xiao@gmail.com>
2023-04-15 19:19:43 +1000

    Removed trailing line.

--- views/constant/prompt.js
+++ views/constant/prompt.js
@@ -5,8 +5,7 @@ export const PRE_DEFINED_PROMPTS = [
 - Proficiency in HTML, CSS, JavaScript, and Python
 - Familiarity with front-end frameworks such as React or Angular
 - Experience with back-end technologies like Django or Node.js
-- Strong problem-solving and communication skills
-`
+- Strong problem-solving and communication skills`
   },
   {
     title: 'Create an OpenAPI spec for a REST API', prompt: `Design an OpenAPI schema for a simple task management API with the following endpoints:
@@ -19,8 +18,7 @@ export const PRE_DEFINED_PROMPTS = [
 The Task model should have the following fields:
 - id: A unique identifier
 - title: A short description of the task
-- completed: A boolean indicating if the task is completed
-`
+- completed: A boolean indicating if the task is completed`
   },
   {
     title: 'Compare Azure and GCP', prompt: `Compare Azure Functions and Google Cloud Functions in terms of the following aspects:
@@ -29,7 +27,6 @@ The Task model should have the following fields:
 - Scalability
 - Deployment options
 - Supported programming languages
-Please create a table.
-`
+Please create a table.`
   },
 ]
