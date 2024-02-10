6682005469da068b362d64bfdc3c4692df51feab
MrCoder <eagle.xiao@gmail.com>
2024-02-10 15:59:24 +1100

    Use sequlize for v2/client/info.

--- service/db.js
+++ service/db.js
@@ -5,42 +5,44 @@ const sequelize = new Sequelize({
   storage: 'database.sqlite'
 });
 
+export const Client = sequelize.define('gpt_dock_client_data', {
+  client_id: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    primaryKey: true
+  },
+  product_id: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    primaryKey: true
+  },
+  _updated_at: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+  },
+  max_quota: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 500000
+  },
+  token_quota: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 500000
+  },
+  version: {
+    type: Sequelize.BIGINT,
+    allowNull: false
+  }
+}, {
+  timestamps: false,
+  underscored: true
+});
+
+
 async function init() {
 
-  const Client = sequelize.define('gpt_dock_client_data', {
-    client_id: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      primaryKey: true
-    },
-    product_id: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      primaryKey: true
-    },
-    _updated_at: {
-      type: Sequelize.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-    },
-    max_quota: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      defaultValue: 500000
-    },
-    token_quota: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      defaultValue: 500000
-    },
-    version: {
-      type: Sequelize.BIGINT,
-      allowNull: false
-    }
-  }, {
-    timestamps: false,
-    underscored: true
-  });
 
   await Client.sync();
 
