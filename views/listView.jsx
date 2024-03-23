200ac4d4052c505e245c570443e3c968dec69528
jack <jack@xmind.net>
2024-03-23 08:58:11 +0800

    #73: add margin

--- views/listView.jsx
+++ views/listView.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import DebugComponent from './components/DebugComponent';
 import Button from '@atlaskit/button';
-import { v4 as uuidv4 } from 'uuid';
-import { observeDomChanges } from '../utils';
 
+import { observeDomChanges } from '../utils';
 const Page = styled.div`
   display: flex;
   flex-direction: column;
@@ -19,7 +18,7 @@ const Wrapper = styled.div`
   display: flex;
   height: 100%;
   width: 100%;
-  margin: 0 auto;
+  margin: 20 auto;
   flex-direction: column;
   flex-grow: 1;
   justify-content: space-between;
@@ -32,79 +31,75 @@ const Wrapper = styled.div`
 const customContentKey = 'ac:gptdock-confluence:gpt-custom-content-key';
 
 const ListView = () => {
+  const [confDiagramResp, setConfDiagramResp] = useState([]);
+  const [diagramItemList, setDiagramItemList] = useState([]);
+  const [paginationObj, setPaginationObj] = useState({
+    hasPrev: false,
+    hasNext: true,
+    start: 0,
+    limit: 25,
+    size: 0,
+  });
 
-	const [confDiagramResp, setConfDiagramResp] = useState([]);
-	const [diagramItemList, setDiagramItemList] = useState([]);
-	const [paginationObj, setPaginationObj] = useState({
-		hasPrev: false,
-		hasNext: true,
-		start: 0,
-		limit: 25,
-		size: 0
-	});
+  async function loadDiagramListFromConfluence() {
+    const localAp = AP;
+    const context = await localAp.context.getContext();
+    localAp.request({
+      url: '/rest/api/content/',
+      type: 'GET',
+      data: {
+        type: customContentKey,
+        spaceKey: context.confluence.space.key,
+        expand: 'version',
+      },
+      success: function (response) {
+        let respJson = JSON.parse(response);
+        setPaginationObj({
+          hasPrev: respJson._links.prev != undefined,
+          hasNext: respJson._links.next != undefined,
+          start: respJson.start,
+          limit: respJson.limit,
+          size: respJson.size,
+        });
+        setConfDiagramResp(respJson.results);
+        setTimeout(localAp.resize, 1000);
+      },
+      error: function (err) {
+        console.log('err - ', err);
+      },
+    });
+  }
 
-	async function loadDiagramListFromConfluence() {
-		  const localAp = AP;
-			const context = await localAp.context.getContext();
-			localAp.request({
-        url: "/rest/api/content/",
-        type: "GET",
-        data: {
-          "type": customContentKey,
-          "spaceKey": context.confluence.space.key,
-          "expand": "version"
-        },
-        success: function (response) {
-          let respJson = JSON.parse(response);
-          setPaginationObj({
-            hasPrev: respJson._links.prev != undefined,
-            hasNext: respJson._links.next != undefined,
-            start: respJson.start,
-            limit: respJson.limit,
-            size: respJson.size
-          });
-          setConfDiagramResp(respJson.results);
-          setTimeout(localAp.resize, 1000);
-        },
-        error: function (err) {
-          console.log("err - ", err)
-        }
-      });
-	}
-
-
-	async function loadPageData(start, limit) {
-		  const localAp = AP;
-			const context = await localAp.context.getContext();
-			localAp.request({
-        url: "/rest/api/content/",
-        type: "GET",
-        data: {
-          "type": customContentKey,
-          "spaceKey": context.confluence.space.key,
-          "expand": "version",
-          "start": start,
-          "limit": limit
-        },
-        success: function (response) {
-          let respJson = JSON.parse(response);
-          setPaginationObj({
-            hasPrev: respJson._links.prev != undefined,
-            hasNext: respJson._links.next != undefined,
-            start: respJson.start,
-            limit: respJson.limit,
-            size: respJson.size
-          });
-          setConfDiagramResp(respJson.results);
-          setTimeout(localAp.resize, 1000);
-        },
-        error: function (err) {
-          console.log("err - ", err)
-        }
-      });
-	}
-
-
+  async function loadPageData(start, limit) {
+    const localAp = AP;
+    const context = await localAp.context.getContext();
+    localAp.request({
+      url: '/rest/api/content/',
+      type: 'GET',
+      data: {
+        type: customContentKey,
+        spaceKey: context.confluence.space.key,
+        expand: 'version',
+        start: start,
+        limit: limit,
+      },
+      success: function (response) {
+        let respJson = JSON.parse(response);
+        setPaginationObj({
+          hasPrev: respJson._links.prev != undefined,
+          hasNext: respJson._links.next != undefined,
+          start: respJson.start,
+          limit: respJson.limit,
+          size: respJson.size,
+        });
+        setConfDiagramResp(respJson.results);
+        setTimeout(localAp.resize, 1200);
+      },
+      error: function (err) {
+        console.log('err - ', err);
+      },
+    });
+  }
 
   useEffect(() => {
     loadDiagramListFromConfluence();
@@ -113,48 +108,54 @@ const ListView = () => {
   }, []);
 
   useEffect(() => {
-    setDiagramItemList(confDiagramResp
-      .sort((doc1, doc2) => Number(doc2.id) - Number(doc1.id))
-      .map((doc) => {
-        return (
-          <tr key={doc.id}>
-            <td>{doc.title}</td>
-          </tr>
-        )
-      }))
+    setDiagramItemList(
+      confDiagramResp
+        .sort((doc1, doc2) => Number(doc2.id) - Number(doc1.id))
+        .map((doc) => {
+          return (
+            <tr key={doc.id}>
+              <td>{doc.title}</td>
+            </tr>
+          );
+        })
+    );
   }, [confDiagramResp]);
 
-
   const turnNextPage = (e) => {
-      e.preventDefault();
-			loadPageData(paginationObj.start + paginationObj.limit, paginationObj.limit);
+    e.preventDefault();
+    loadPageData(
+      paginationObj.start + paginationObj.limit,
+      paginationObj.limit
+    );
   };
 
   const turnPrevPage = (e) => {
-      e.preventDefault();
-			loadPageData(paginationObj.start - paginationObj.limit, paginationObj.limit);
+    e.preventDefault();
+    loadPageData(
+      paginationObj.start - paginationObj.limit,
+      paginationObj.limit
+    );
   };
 
   return (
     <Page>
       <DebugComponent />
       <Wrapper>
-        <h1>Diagramly Collection</h1>
+      <div style={{margin: "40px"}}>
+      <h1>Diagramly Collection</h1>
 				<table>
-          <tbody>
-            {diagramItemList}
-          </tbody>
+          <tbody>{diagramItemList}</tbody>
         </table>
-				<div>
-					{ paginationObj.hasPrev &&
-						<Button onClick={turnPrevPage}>Prev</Button> 
-					}
-					{ paginationObj.hasNext &&
-						<Button onClick={turnNextPage}>Next</Button> 
-					}
-
-				</div>
-      </Wrapper>
+        <div>
+          {paginationObj.hasPrev && (
+            <Button onClick={turnPrevPage}>Prev</Button>
+          )}
+          {paginationObj.hasNext && (
+            <Button onClick={turnNextPage}>Next</Button>
+          )}
+        </div>
+      </div>
+       </Wrapper>
     </Page>
   );
 };
